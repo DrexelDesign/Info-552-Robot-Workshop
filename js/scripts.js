6c0837fae82dd955cd3bc14e6b4932848385e6cc
Josh <joshGdovindesigns.com>
2023-08-20 08:42:55 -0400

    added fancybox

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,52 @@
-Fancybox.bind('[data-fancybox="gallery"]',{
-        Thumbs : {
-          type: "modern"
-        }
+// FancyBox
+Fancybox.bind('[data-fancybox]', {
+	// Your custom options
+	Thumbs : {
+	  type: "modern"
+	}
 });
 
-
 let burgerBtn = document.querySelector("#burger-btn");
-let navContainer = document.querySelector("#nav-container")
+let navContainer = document.querySelector("#nav-container");
 
 let canISeeTheMenu = false;
 
 // click function
 burgerBtn.addEventListener("click", () => {
-    // console.log("Button is working!!!");
+	// console.log("Button is working!!!");
 
-    burgerBtn.classList.toggle("open");
-    navContainer.classList.toggle("showNavContainer");
-    canISeeTheMenu.toggle();
+	burgerBtn.classList.toggle("open");
+	navContainer.classList.toggle("showNavContainer");
+	canISeeTheMenu.toggle();
 
-    // if(canISeeTheMenu == false){
-    //     console.log("Show Menu");
-    //     // adds showNavContainer display block class
-    //     navContainer.classList.add("showNavContainer");
+	// if(canISeeTheMenu == false){
+	//     console.log("Show Menu");
+	//     // adds showNavContainer display block class
+	//     navContainer.classList.add("showNavContainer");
 
-    //     // adds the open class to the burger icon
-    //     // burgerBtn.classList.add("open");
+	//     // adds the open class to the burger icon
+	//     // burgerBtn.classList.add("open");
 
-    //     // sets canISeeTheMenu var to true
-    //     canISeeTheMenu = true;
-    // } else {
-    //    // removes showNavContainer display block class
-    // //    navContainer.classList.remove("showNavContainer");
+	//     // sets canISeeTheMenu var to true
+	//     canISeeTheMenu = true;
+	// } else {
+	//    // removes showNavContainer display block class
+	// //    navContainer.classList.remove("showNavContainer");
 
-    //     // removes the open class to the burger icon
-    //     // burgerBtn.classList.remove("open");
+	//     // removes the open class to the burger icon
+	//     // burgerBtn.classList.remove("open");
 
-    //    // sets canISeeTheMenu var to false
-    //    canISeeTheMenu = false;
-    // }
-})
+	//    // sets canISeeTheMenu var to false
+	//    canISeeTheMenu = false;
+	// }
+});
 
-navContainer.addEventListener("click", () =>{
-    // removes showNavContainer display block class
-    navContainer.classList.remove("showNavContainer");
+navContainer.addEventListener("click", () => {
+	// removes showNavContainer display block class
+	navContainer.classList.remove("showNavContainer");
 
-    burgerBtn.classList.remove("open");
+	burgerBtn.classList.remove("open");
 
-    // sets canISeeTheMenu var to false
-    canISeeTheMenu = false;
-})
+	// sets canISeeTheMenu var to false
+	canISeeTheMenu = false;
+});
